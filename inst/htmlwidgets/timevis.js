18d39b4e058076aafb4af5ff7bc3050957a1cad2
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-10-13 19:25:56 +1100

    Revert "Try module import syntax"
    This reverts commit c63a7d8287b5145257f91d23fb89177540d74665.

--- inst/htmlwidgets/timevis.js
+++ inst/htmlwidgets/timevis.js
@@ -3,8 +3,6 @@
 /* timevis                                                           */
 /* Create timeline visualizations in R using htmlwidgets and vis.js  */
 /*********************************************************************/
-import { Timeline } from "vis-timeline";
-
 
 HTMLWidgets.widget({
 
